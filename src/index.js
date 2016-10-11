706a1082f7b2c2c41a3592f1a6adabdee2784821
Chris Day <chris.p.day@gmail.com>
2016-10-12 07:17:07 +1100

    add module and export decl for .d.ts

--- src/index.js
+++ src/index.js
@@ -9,90 +9,95 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 const parseFunction = require("parse-function");
 const tedious = require("tedious");
-var SqlType;
-(function (SqlType) {
-    SqlType[SqlType["Boolean"] = 0] = "Boolean";
-    SqlType[SqlType["String"] = 1] = "String";
-    SqlType[SqlType["Number"] = 2] = "Number";
-})(SqlType || (SqlType = {}));
-var StatementType;
-(function (StatementType) {
-    StatementType[StatementType["Where"] = 0] = "Where";
-})(StatementType || (StatementType = {}));
-class SqlServerGenerator {
-    Execute(dbSet) {
-        return __awaiter(this, void 0, void 0, function* () {
-            const columns = dbSet.dbSetConfiguration.ColumnConfigurations.map(c => `[${c.Name}]`).join(",");
-            const table = `[${dbSet.dbSetConfiguration.Database}].[${dbSet.dbSetConfiguration.Schema}].[${dbSet.dbSetConfiguration.Name}]`;
-            const sql = `select ${columns} from ${table}`;
-            console.log(sql);
-            const conn = new tedious.Connection({ server: "localhost", userName: "testuser", password: "testpassword", options: { useColumnNames: true, rowCollectionOnRequestCompletion: true } });
-            let connPromise = new Promise((resolve, reject) => {
-                conn.on("connect", err => {
-                    if (err) {
-                        reject(err);
-                    }
-                    resolve();
-                });
-            });
-            let requestPromise = connPromise.then(() => {
-                return new Promise((resolve, reject) => {
-                    const request = new tedious.Request(sql, (err, rowCount, rows) => {
+var LinqToSql;
+(function (LinqToSql) {
+    (function (SqlType) {
+        SqlType[SqlType["Boolean"] = 0] = "Boolean";
+        SqlType[SqlType["String"] = 1] = "String";
+        SqlType[SqlType["Number"] = 2] = "Number";
+    })(LinqToSql.SqlType || (LinqToSql.SqlType = {}));
+    var SqlType = LinqToSql.SqlType;
+    (function (StatementType) {
+        StatementType[StatementType["Where"] = 0] = "Where";
+    })(LinqToSql.StatementType || (LinqToSql.StatementType = {}));
+    var StatementType = LinqToSql.StatementType;
+    class SqlServerGenerator {
+        Execute(dbSet) {
+            return __awaiter(this, void 0, void 0, function* () {
+                const columns = dbSet.dbSetConfiguration.ColumnConfigurations.map(c => `[${c.Name}]`).join(",");
+                const table = `[${dbSet.dbSetConfiguration.Database}].[${dbSet.dbSetConfiguration.Schema}].[${dbSet.dbSetConfiguration.Name}]`;
+                const sql = `select ${columns} from ${table}`;
+                console.log(sql);
+                const conn = new tedious.Connection({ server: "localhost", userName: "testuser", password: "testpassword", options: { useColumnNames: true, rowCollectionOnRequestCompletion: true } });
+                let connPromise = new Promise((resolve, reject) => {
+                    conn.on("connect", err => {
                         if (err) {
                             reject(err);
                         }
-                        conn.close();
-                        resolve(rows);
+                        resolve();
                     });
-                    request.on("row", columns => { });
-                    conn.execSql(request);
                 });
+                let requestPromise = connPromise.then(() => {
+                    return new Promise((resolve, reject) => {
+                        const request = new tedious.Request(sql, (err, rowCount, rows) => {
+                            if (err) {
+                                reject(err);
+                            }
+                            conn.close();
+                            resolve(rows);
+                        });
+                        request.on("row", columns => { });
+                        conn.execSql(request);
+                    });
+                });
+                return requestPromise;
             });
-            return requestPromise;
-        });
+        }
     }
-}
-class DbSet {
-    constructor(dbSetConfiguration, sqlGenerator) {
-        this.dbSetConfiguration = dbSetConfiguration;
-        this.sqlGenerator = sqlGenerator;
-        this.statements = [];
+    LinqToSql.SqlServerGenerator = SqlServerGenerator;
+    class DbSet {
+        constructor(dbSetConfiguration, sqlGenerator) {
+            this.dbSetConfiguration = dbSetConfiguration;
+            this.sqlGenerator = sqlGenerator;
+            this.statements = [];
+        }
+        Select(f) {
+            this.statements = this.statements.concat([{
+                    statementType: StatementType.Where,
+                    statement: '' + f,
+                    parsed: parseFunction(f)
+                }]);
+            return this;
+        }
+        ToArray() {
+            return __awaiter(this, void 0, void 0, function* () {
+                return yield this.sqlGenerator.Execute(this);
+            });
+        }
+        toString() {
+            return this.statements.map(s => "" + s.statementType + "->" + s.statement + "->" + JSON.stringify(s.parsed)).join("\\r\\n");
+        }
     }
-    Select(f) {
-        this.statements = this.statements.concat([{
-                statementType: StatementType.Where,
-                statement: '' + f,
-                parsed: parseFunction(f)
-            }]);
-        return this;
-    }
-    ToArray() {
-        return __awaiter(this, void 0, void 0, function* () {
-            return yield this.sqlGenerator.Execute(this);
-        });
-    }
-    toString() {
-        return this.statements.map(s => "" + s.statementType + "->" + s.statement + "->" + JSON.stringify(s.parsed)).join("\\r\\n");
-    }
-}
+    LinqToSql.DbSet = DbSet;
+})(LinqToSql = exports.LinqToSql || (exports.LinqToSql = {}));
 class Table1 {
 }
 class LinqToSqlContext {
     constructor() {
     }
     Table1() {
-        return new DbSet({
+        return new LinqToSql.DbSet({
             Database: "LinqToSql",
             Schema: "dbo",
             Name: "Table1",
             ColumnConfigurations: [
-                { Name: "Id", DataType: SqlType.Number },
-                { Name: "Number", DataType: SqlType.Number },
-                { Name: "String", DataType: SqlType.String },
-                { Name: "Boolean", DataType: SqlType.Boolean },
-                { Name: "Text", DataType: SqlType.String },
+                { Name: "Id", DataType: LinqToSql.SqlType.Number },
+                { Name: "Number", DataType: LinqToSql.SqlType.Number },
+                { Name: "String", DataType: LinqToSql.SqlType.String },
+                { Name: "Boolean", DataType: LinqToSql.SqlType.Boolean },
+                { Name: "Text", DataType: LinqToSql.SqlType.String },
             ]
-        }, new SqlServerGenerator());
+        }, new LinqToSql.SqlServerGenerator());
     }
 }
 var context = new LinqToSqlContext();
--- src/index.ts
+++ src/index.ts
@@ -1,100 +1,105 @@
 import parseFunction = require("parse-function");
 import tedious = require("tedious");
 
-type Func<T, U> = (t: T) => U;
+export module LinqToSql {
 
-interface IQueryable<T> {
-    Select(f: Func<T, T>): IQueryable<T>;
-    ToArray(): Promise<Array<T>>;
-    toString(): string;
-}
+    export type Func<T, U> = (t: T) => U;
 
-enum SqlType {
-    Boolean,
-    String,
-    Number
-}
+    export interface IQueryable<T> {
+        Select(f: Func<T, T>): IQueryable<T>;
+        ToArray(): Promise<Array<T>>;
+        toString(): string;
+    }
 
-interface IDbColumnConfiguration {
-    Name: string;
-    DataType: SqlType;
-}
+    export enum SqlType {
+        Boolean,
+        String,
+        Number
+    }
 
-interface IDbSetConfiguration {
-    Database: string;
-    Schema: string;
-    Name: string;
-    ColumnConfigurations: IDbColumnConfiguration[];
-}
+    export interface IDbColumnConfiguration {
+        Name: string;
+        DataType: SqlType;
+    }
 
-enum StatementType {
-    Where
-}
+    export interface IDbSetConfiguration {
+        Database: string;
+        Schema: string;
+        Name: string;
+        ColumnConfigurations: IDbColumnConfiguration[];
+    }
 
-interface IDbSetStatement {
-    statementType: StatementType,
-    statement: string,
-    parsed: any
-}
+    export enum StatementType {
+        Where
+    }
 
-interface ISqlGenerator {
-    Execute<T>(dbSet: DbSet<T>): Promise<Array<T>>;
-}
+    export interface IDbSetStatement {
+        statementType: StatementType,
+        statement: string,
+        parsed: any
+    }
 
-class SqlServerGenerator implements ISqlGenerator {
-    async Execute<T>(dbSet: DbSet<T>): Promise<Array<T>> {
-        const columns = dbSet.dbSetConfiguration.ColumnConfigurations.map(c => `[${c.Name}]`).join(",");
-        const table = `[${dbSet.dbSetConfiguration.Database}].[${dbSet.dbSetConfiguration.Schema}].[${dbSet.dbSetConfiguration.Name}]`;
-        const sql = `select ${columns} from ${table}`;
-        console.log(sql);
-        const conn = new tedious.Connection({ server: "localhost", userName: "testuser", password: "testpassword", options: { useColumnNames: true, rowCollectionOnRequestCompletion: true }});
-        let connPromise = new Promise((resolve, reject) => {
-            conn.on("connect", err => {
-                if (err) {
-                    reject(err);
-                }
-                resolve();
-            });
-        });
-        let requestPromise = connPromise.then(() => {
-           return new Promise<Array<T>>((resolve, reject) => {
-                const request = new tedious.Request(sql, (err, rowCount, rows) => {
+    export interface ISqlGenerator {
+        Execute<T>(dbSet: DbSet<T>): Promise<Array<T>>;
+    }
+
+    export class SqlServerGenerator implements ISqlGenerator {
+        async Execute<T>(dbSet: DbSet<T>): Promise<Array<T>> {
+            const columns = dbSet.dbSetConfiguration.ColumnConfigurations.map(c => `[${c.Name}]`).join(",");
+            const table = `[${dbSet.dbSetConfiguration.Database}].[${dbSet.dbSetConfiguration.Schema}].[${dbSet.dbSetConfiguration.Name}]`;
+            const sql = `select ${columns} from ${table}`;
+            console.log(sql);
+            const conn = new tedious.Connection({ server: "localhost", userName: "testuser", password: "testpassword", options: { useColumnNames: true, rowCollectionOnRequestCompletion: true } });
+            let connPromise = new Promise((resolve, reject) => {
+                conn.on("connect", err => {
                     if (err) {
                         reject(err);
                     }
-                    conn.close();
-                    resolve(rows);
+                    resolve();
                 });
-                request.on("row", columns => { });
-                conn.execSql(request);
-           }); 
-        });
-        return requestPromise;
-    }
-}
-
-class DbSet<T> implements IQueryable<T> {
-    public statements: IDbSetStatement[] = [];
-
-    constructor(public dbSetConfiguration: IDbSetConfiguration, public sqlGenerator: ISqlGenerator) {
-    }
-
-    Select(f: Func<T, T>): IQueryable<T> {
-        this.statements = this.statements.concat([{ 
-            statementType: StatementType.Where, 
-            statement: '' + f,
-            parsed: parseFunction(f) 
-        }]);
-        return this;
+            });
+            let requestPromise = connPromise.then(() => {
+                return new Promise<Array<T>>((resolve, reject) => {
+                    const request = new tedious.Request(sql, (err, rowCount, rows) => {
+                        if (err) {
+                            reject(err);
+                        }
+                        conn.close();
+                        resolve(rows);
+                    });
+                    request.on("row", columns => { });
+                    conn.execSql(request);
+                });
+            });
+            return requestPromise;
+        }
     }
 
-    async ToArray(): Promise<Array<T>> {
-        return await this.sqlGenerator.Execute<T>(this);
+    export class DbSet<T> implements IQueryable<T> {
+        public statements: IDbSetStatement[] = [];
+
+        constructor(public dbSetConfiguration: IDbSetConfiguration, public sqlGenerator: ISqlGenerator) {
+        }
+
+        Select(f: Func<T, T>): IQueryable<T> {
+            this.statements = this.statements.concat([{
+                statementType: StatementType.Where,
+                statement: '' + f,
+                parsed: parseFunction(f)
+            }]);
+            return this;
+        }
+
+        async ToArray(): Promise<Array<T>> {
+            return await this.sqlGenerator.Execute<T>(this);
+        }
+
+        toString(): string {
+            return this.statements.map(s => "" + s.statementType + "->" + s.statement + "->" + JSON.stringify(s.parsed)).join("\\r\\n");
+        }
     }
 
-    toString() : string {
-        return this.statements.map(s => "" + s.statementType + "->" + s.statement + "->" + JSON.stringify(s.parsed)).join("\\r\\n");
-    }
+
 }
 
 class Table1 {
@@ -108,22 +113,21 @@ class LinqToSqlContext {
     constructor() {
     }
 
-    Table1(): IQueryable<Table1> {
-        return new DbSet<Table1>({
+    Table1(): LinqToSql.IQueryable<Table1> {
+        return new LinqToSql.DbSet<Table1>({
             Database: "LinqToSql",
             Schema: "dbo",
             Name: "Table1",
             ColumnConfigurations: [
-                { Name: "Id", DataType: SqlType.Number },
-                { Name: "Number", DataType: SqlType.Number },
-                { Name: "String", DataType: SqlType.String },
-                { Name: "Boolean", DataType: SqlType.Boolean },
-                { Name: "Text", DataType: SqlType.String },
+                { Name: "Id", DataType: LinqToSql.SqlType.Number },
+                { Name: "Number", DataType: LinqToSql.SqlType.Number },
+                { Name: "String", DataType: LinqToSql.SqlType.String },
+                { Name: "Boolean", DataType: LinqToSql.SqlType.Boolean },
+                { Name: "Text", DataType: LinqToSql.SqlType.String },
             ]
-        }, new SqlServerGenerator());
+        }, new LinqToSql.SqlServerGenerator());
     }
 }
-
 var context = new LinqToSqlContext();
 var rows = context.Table1().Select(t => t).ToArray();
 rows.then(rs => console.log("rows " + JSON.stringify(rs)));
